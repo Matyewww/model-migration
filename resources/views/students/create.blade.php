ace710bcccd35bd1c5918a5f5b14c9172b0c7ad9
ReinzeeMathewChua <chuarc.962.stud@cdd.edu.ph>
2024-07-31 11:50:21 +0800

    model validation

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -16,30 +16,52 @@
         <div class="form-group mb-3">
         <label class="form-label">First Name</label>
         <input type="text" name="fname" class="form-controler"> </div>
+        @error('fname')
+        <div>{{ $message }}</div>
+        @enderror
+
         <div class="form-group mb-3">
         <label class="form-label">Last Name</label>
         <input type="text" name="lname" class="form-controler">
+        @error('lname')
+        <div>{{ $message }}</div>
+        @enderror
         </div>
         <div class="form-group mb-3">
         <label class="form-label">Phone Number</label>
         <input type="text" name="phone" class="form-controler">
+        @error('phone')
+        <div>{{ $message }}</div>
+        @enderror
         </div>
         <div class="form-group mb-3">
         <label class="form-label">Address</label>
         <input type="text" name="address" class="form-controler">
+        @error('address')
+        <div>{{ $message }}</div>
+        @enderror
         </div>
         <div class="form-group mb-3">
         <label class="form-label">City</label>
         <input type="text" name="city" class="form-controler">
+        @error('city')
+        <div>{{ $message }}</div>
+        @enderror
         </div>
         <div class="form-group mb-3">
         <label class="form-label">email</label>
         <input type="text" name="email" class="form-controler">
+        @error('email')
+        <div>{{ $message }}</div>
+        @enderror
         </div>
         
         <div class="form-group mb-3">
         <label class="form-label">Birthdate</label>
         <input type="date" name="date" class="form-controler">
+        @error('birthdate')
+        <div>{{ $message }}</div>
+        @enderror
         </div>
         <div class="form-group mb-3">
             <button class="btn btn-primary">Save Changes</button>
